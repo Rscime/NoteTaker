4c45f68b63de3a0a418edaef253670bd5d04de8a
Rscime <Rachelscime@gmail.com>
2023-08-25 18:10:35 -0400

    basic html requests skipping over the index js file

---
+++ Develop/routes/html.js
@@ -0,0 +1,16 @@
+// requierments
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/notes', (req, res) => {
+    console.info(`${req.method} request received to view notes page`);
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('/', (req, res) => {
+    console.info(`${req.method} request received to view home page`);
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -8,9 +8,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 //pull in API and HTML routes
-const apiRouter = require('./routes/api');
-const htmlRouter = require('./routes/html');
-app.use(apiRouter);
-app.use(htmlRouter);
+const apiRoute = require('./routes/api');
+const htmlRoute = require('./routes/html');
+app.use(apiRoute);
+app.use(htmlRoute);
 
 app.listen(PORT, () => console.log(`Listening on PORT, at http://localhost:${PORT} 🚀`))
