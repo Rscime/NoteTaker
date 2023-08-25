e4d1000c81cdd5016e9142254bca524d18a11a80
Rscime <Rachelscime@gmail.com>
2023-08-25 10:50:54 -0400

    rough outline almost done need to finfish API methods

---
+++ Develop/routes/api.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router()
+
+const db = require('../db/db.json')
+
+//GET all notes...aka read the DB file
+
+
+
+//POST to DB in JSON obj
+//need to find nmp that auto incs for posts
+
+
+
+//optional delete route
+
+
+module.exports = router;
