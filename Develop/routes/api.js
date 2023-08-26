eb560517f8f5592c64b84d47e546709b3ccc9e30
Rscime <Rachelscime@gmail.com>
2023-08-26 13:24:24 -0400

    updated with asynch functions

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,17 +1,18 @@
 // requierments, ect
 const express = require('express');
 const router = express.Router();
-const db = require('../db/db.json');
+const db = require('path').join(__dirname, '../db/db.json');
 
 // connect to fs functions to read and write to DB file
 const fs = require('fs');
 const { readFile, writeFile } = require('fs/promises');
 const util = require('util').promisify;
 
+
 //GET aka read all notes @ /api/notes
-router.get('api/notes', (req, res) => {
+router.get("/api/notes", (req, res) => {
     console.info(`${req.method} request received to view all`);
-    const notes = readFile(db,'utf-8');
+    const notes = fs.readFile(db,'utf-8');
     return res.json(notes);
 })
 
@@ -24,8 +25,8 @@ router.post('/api/notes', (req,res) => {
         title: req.body.title,
         text: req.body.text,
     };
-    writeFile(db, JSON.stringify(newNote));
-    const notes = readFile(db,'utf-8');
+    fs.writeFile(db, JSON.stringify(newNote));
+    const notes = fs.readFile(db,'utf-8');
     return res.json(notes);  
 })
 
--- routes/api.js
+++ routes/api.js
@@ -1,32 +1,32 @@
 // requierments, ect
 const express = require('express');
 const router = express.Router();
-const db = require('../db/db.json');
+const db = require('path').join(__dirname, '../db/db.json');
 
 // connect to fs functions to read and write to DB file
 const fs = require('fs');
-const { readFile, writeFile } = require('fs/promises');
-const util = require('util').promisify;
 
 //GET aka read all notes @ /api/notes
-router.get('api/notes', (req, res) => {
-    console.info(`${req.method} request received to view all`);
-    const notes = readFile(db,'utf-8');
+router.get("/api/notes", async (req, res) => {
+    console.info(`${req.method} request received to view all notes`);
+    const allNotes = await fs.readFile(db,'utf-8');
+    const notes = JSON.parse(allNotes);
     return res.json(notes);
 })
 
 
 //POST to notes @ /api/notes
 //** need to add in a incrementor */
-router.post('/api/notes', (req,res) => {
+router.post('/api/notes', async (req,res) => {
     console.info(`${req.method} request received to add a note`);
     let newNote = {
         title: req.body.title,
         text: req.body.text,
     };
-    writeFile(db, JSON.stringify(newNote));
-    const notes = readFile(db,'utf-8');
-    return res.json(notes);  
+    await fs.writeFile(db, JSON.stringify(newNote));
+    const allNotes = await fs.readFile(db,'utf-8');
+    const notes = JSON.parse(allNotes);
+    return res.json(notes); 
 })
 
 
